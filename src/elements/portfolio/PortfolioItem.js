6d303aade1565792382b2d3396082b3b7729a3be
asinchenko <wonderfulsanchez@gmail.com>
2023-02-27 11:34:31 +0600

    pictures

--- src/elements/portfolio/PortfolioItem.js
+++ src/elements/portfolio/PortfolioItem.js
@@ -7,15 +7,15 @@ const PortfolioItem = ({ portfolio }) => {
             <div className="inner">
                 <div className="thumbnail">
                     <figure className="card-image">
-                        <Link to={process.env.PUBLIC_URL + `/portfolio-details/${portfolio.id}`}>
+                        <Link to={process.env.PUBLIC_URL + `/contact`}>
                             <img className="img-fluid" src={portfolio.portfolioImage} alt="Portfolio-01"/>
                         </Link>
                     </figure>
-                    <Link to={process.env.PUBLIC_URL + `/portfolio-details/${portfolio.id}`} className="rwt-overlay"></Link>
+                    <Link to={process.env.PUBLIC_URL + `/contact`} className="rwt-overlay"></Link>
                 </div>
                 <div className="content">
                     <h5 className="title mb--10">
-                        <Link to={process.env.PUBLIC_URL + `/portfolio-details/${portfolio.id}`}>
+                        <Link to={process.env.PUBLIC_URL + `/contact`}>
                             {portfolio.title}
                         </Link>
                     </h5>
